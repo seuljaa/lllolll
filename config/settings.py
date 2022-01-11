81b194b4594684182f25b0d2bed747353c261b51
seuljaa <seuljaa940503@gmail.com>
2022-01-11 21:17:05 +0900

    파일추가

--- config/settings.py
+++ config/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'debug_toolbar',
     # 로컬
     'deal.apps.DealConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
--- deal/models.py
+++ deal/models.py
@@ -1,4 +1,20 @@
 from django.db import models
+from accounts.models import User
 
 # Create your models here.
 
+class DealItems(models.Model):
+    create_date = models.DateTimeField('등록날짜', auto_now_add=True)
+    update_date = models.DateTimeField('갱신날짜', auto_now=True)
+    is_complete = models.BooleanField('완료여부', default=False)
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    name = models.CharField('상품명(내부용)', max_length=100)
+    display_name = models.CharField('상품명(고객용)', max_length=100)
+    price = models.PositiveIntegerField('권장판매가')
+    sale_price = models.PositiveIntegerField('실제판매가')
+    is_hidden = models.BooleanField('노출여부', default=False)
+    is_sold_out = models.BooleanField('품절여부', default=False)
+    category_id = models.PositiveIntegerField('카테고리번호', default=0)
+    hit_count = models.PositiveIntegerField('조회수', default=0)
+    review_count = models.PositiveIntegerField('리뷰수', default=0)
+    review_point = models.PositiveIntegerField('리뷰평점', default=0)
