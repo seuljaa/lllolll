e622215770ee81970b210aacfb42375df45c57aa
seuljaa <seuljaa940503@gmail.com>
2022-01-12 12:42:17 +0900

    모델수정

--- deal/models.py
+++ deal/models.py
@@ -4,6 +4,19 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class DealItems(models.Model):
+    STAR_FORCE_CHOICES = (
+        ('na', '해당없음'),
+        ('under_17', '17성 이하'),
+        ('up_17', '17성 초과'),
+    )
+
+    ABILITY_CHOICES = (
+        ('na', '해당없음'),
+        ('epic', '에픽'),
+        ('unique', '유니크'),
+        ('legendary', '레전드리'),
+    )
+
     user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     subject = models.CharField('제목', max_length=100)
     create_date = models.DateTimeField('등록날짜', auto_now_add=True)
@@ -13,24 +26,8 @@ class DealItems(models.Model):
     part = models.CharField('부위', max_length=20)
     price = models.PositiveIntegerField('가격')
     price_kind = models.BooleanField('억/엄', default=True)
-
-    class Star_Force(models.Model):
-        STAR_FORCE_CHOICES = (
-            ('na', '해당없음'),
-            ('under_17', '17성 이하'),
-            ('up_17', '17성 초과'),
-        )
-        star_force =models.CharField(max_length=9, choices= STAR_FORCE_CHOICES)
-
-    class Ability(models.Model):
-        ABILITY_CHOICES = (
-            ('na', '해당없음'),
-            ('epic', '에픽'),
-            ('unique', '유니크'),
-            ('legendary', '레전드리'),
-        )
-        ability_1 = models.CharField(max_length=9, choices= ABILITY_CHOICES)
-        ability_2 = models.CharField(max_length=9, choices= ABILITY_CHOICES)
-
+    star_force = models.CharField(max_length=9, choices=STAR_FORCE_CHOICES)
+    ability_1 = models.CharField(max_length=9, choices=ABILITY_CHOICES)
+    ability_2 = models.CharField(max_length=9, choices=ABILITY_CHOICES)
     content = models.TextField()
     is_complete = models.BooleanField('완료여부', default=False)
--- deal/views.py
+++ deal/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.paginator import Paginator
+from .models import DealItems
+
 # Create your views here.
 
 def main(request):
     return render(request, 'main.html')
-
-def deal_list(request):
-    return HttpResponse('하이')
