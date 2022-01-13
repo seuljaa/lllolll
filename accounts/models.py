d0aca24e2c858ce36e9fcbb62890399c97e2fd00
seuljaa <seuljaa940503@gmail.com>
2022-01-13 10:02:01 +0900

    유저모델 완성

--- accounts/models.py
+++ accounts/models.py
@@ -1,20 +1,6 @@
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
 
-
-class User(AbstractBaseUser):
-# Default : id / password / last_login
-    email = models.EmailField(max_length=100, null=False)
-    username = models.CharField(max_length=20, unique=True)
-    is_staff = models.BooleanField(default=False)
-    is_admin = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=False)
-    is_superuser = models.BooleanField(default=False)
-    date_joined = models.DateTimeField(auto_now_add=True)
-
-    USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = ['email']
-
 class UserManager(BaseUserManager):
     def create_user(self, username, email=None, password=None, **extra_fields):
         user = self.model(
@@ -40,3 +26,26 @@ class UserManager(BaseUserManager):
         superuser.is_staff = True
         superuser.save()
         return superuser
+
+class User(AbstractBaseUser):
+# Default : id / password / last_login
+    email = models.EmailField(max_length=100, null=False)
+    username = models.CharField(max_length=20, unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+    objects = UserManager()
+
+    def has_module_perms(self, app_label):
+        return True
+
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+
