1e96b704510a07e2bb4e322879abaf3a661d35b8
seuljaa <seuljaa940503@gmail.com>
2022-01-13 13:44:54 +0900

    url추가

--- deal/urls.py
+++ deal/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'deal'
 
 urlpatterns = [
-    path('deal_list/', views.deal_list, name='deal_list')
+    path('deal_list/', views.deal_list, name='deal_list'),
+    path('post_Armor/', views.post_Armor, name='post_Armor'),
 ]
