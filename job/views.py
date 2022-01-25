b4cfe4dfbee6cc644929bd3a210a2bb72a2c31d8
seuljaa <seuljaa940503@gmail.com>
2022-01-25 19:32:56 +0900

    필터적용

--- job/views.py
+++ job/views.py
@@ -2,19 +2,28 @@ from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.db.models import Q
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
 
 from .forms import PostForm
 from .models import Post_job
 
+
 def job_list(request):
     page = request.GET.get('page', '1')
     search_keyword = request.GET.get('search_keyword', '')
+    filter_boss = request.GET.get('boss')
+    filter_level = request.GET.get('level')
 
     post_job_list = Post_job.objects.order_by('-create_date')
 
     if search_keyword:
-        post_job_list = Post_job.filter(
+        post_job_list = post_job_list.filter(
             Q(subject__icontains=search_keyword)
+        ).distinct()
+
+    if filter_boss and filter_level:
+        post_job_list = post_job_list.filter(
+            Q(boss__icontains=filter_boss)&Q(level__icontains=filter_level)
         ).distinct()
 
     paginator = Paginator(post_job_list, 10)
@@ -22,6 +31,7 @@ def job_list(request):
 
     context = {'job': page_obj, 'page': page, 'search_keyword': search_keyword}
     return render(request, 'job/job_list.html', context)
+
 
 @login_required(login_url='accounts:sign_in')
 def post_job(request):
@@ -47,4 +57,4 @@ def post_job(request):
 @login_required(login_url='accounts:sign_in')
 def job_detail(request, post_id):
     detail = Post_job.objects.get(pk=post_id)
-    return render(request, 'job/job_detail.html', {'detail':detail})
+    return render(request, 'job/job_detail.html', {'detail': detail})
