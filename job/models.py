d64efdbe705957a10039d192132546a7f10fb109
seuljaa <seuljaa940503@gmail.com>
2022-01-25 11:55:04 +0900

    구인구직리스ㅌ

--- job/models.py
+++ job/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from accounts.models import User
 
 # Create your models here.
+
+class Post_job(models.Model):
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    subject = models.CharField('제목', max_length=100)
+    create_date = models.DateTimeField('등록날짜', auto_now_add=True)
+    update_date = models.DateTimeField('갱신날짜', auto_now=True)
+    server = models.CharField('서버', max_length=10)
+    boss = models.CharField('보스', max_length=20)
+    level = models.CharField('난이도', max_length=10)
+    total = models.PositiveIntegerField('인원')
+    job = models.CharField('직업', max_length=10)
+    price = models.PositiveIntegerField('가격')
+    content = models.TextField()
+    kind = models.BooleanField('구인/구직', default=True)
+    is_complete = models.BooleanField('완료여부', default=False)
--- job/views.py
+++ job/views.py
@@ -1,4 +1,21 @@
+from django.core.paginator import Paginator
+from django.db.models import Q
 from django.shortcuts import render
+from .models import Post_job
 
 def job_list(request):
-    return
+    page = request.GET.get('page', '1')
+    search_keyword = request.GET.get('search_keyword', '')
+
+    post_job_list = Post_job.objects.order_by('-create_date')
+
+    if search_keyword:
+        post_job_list = Post_job.filter(
+            Q(subject__icontains=search_keyword)
+        ).distinct()
+
+    paginator = Paginator(post_job_list, 10)
+    page_obj = paginator.get_page(page)
+
+    context = {'job': page_obj, 'page': page, 'search_keyword': search_keyword}
+    return render(request, 'job/job_list.html', context)
