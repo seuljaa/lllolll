c297fe8e03c55b9235bb6b74320ff6bafa6ce363
seuljaa <seuljaa940503@gmail.com>
2022-01-27 12:41:17 +0900

    어드민

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from accounts.models import User
+from deal.models import DealItems
 
 admin.site.register(User)
+admin.site.register(DealItems)
