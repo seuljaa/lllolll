a01170cefbd0c774f588af9a15f375dfdb8a0cda
seuljaa <seuljaa940503@gmail.com>
2022-02-15 17:20:03 +0900

    비번찾기 구현

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
-from accounts import views
 from django.contrib.auth import views as auth_views
+from django.urls import path, reverse_lazy
+
+from accounts import views
 
 app_name='accounts'
 
@@ -8,9 +9,9 @@ urlpatterns = [
     path('sign_up/', views.sign_up, name='sign_up'),
     path('sign_in/', auth_views.LoginView.as_view(template_name='accounts/sign_in.html'), name='sign_in'),
     path('sign_out/', auth_views.LogoutView.as_view(), name='sign_out'),
-    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(success_url=reverse_lazy('accounts:password_reset_complete'), email_template_name = 'accounts/password_reset_email.html'), name='password_reset'),
     path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(),
+    path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(success_url = reverse_lazy('accounts:password_reset_complete')),
          name='password_reset_confirm'),
-    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(template_name = 'accounts/password_reset_complete.html'), name='password_reset_complete'),
 ]
