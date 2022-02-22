869fadf3e1f632925587df404d641553a8ad9591
seuljaa <seuljaa940503@gmail.com>
2022-02-22 15:00:07 +0900

    어드민 항목 추가

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,7 +4,12 @@ from django.contrib import admin
 from accounts.models import User
 from deal.models import DealItems
 from job.models import Post_job
+from photo.models import Photo_post
+from comment.models import Comment
 
 admin.site.register(User)
 admin.site.register(DealItems)
 admin.site.register(Post_job)
+admin.site.register(Photo_post)
+admin.site.register(Comment)
+
