ac666e812bbb76cfd5b3ca53a748b9cb92ead45e
seuljaa <seuljaa940503@gmail.com>
2022-02-22 19:47:01 +0900

    test

--- config/settings/common.py
+++ config/settings/common.py
@@ -114,6 +114,9 @@ DATABASES = {
         'PORT': '3306',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+            'use_unicode': True,
+
         },
     }
 }
